5d6c7342ede04dc124fd0e611a1d9c776e13b796
Tracycccccc <yanxic37@gmail.com>
2020-12-01 16:51:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/actionmode/Item.java
@@ -0,0 +1,36 @@
+package com.example.actionmode;
+
+
+public class Item {
+
+    private String name;
+    private boolean bo;
+
+    public Item(String name, boolean bo){
+        this.name = name;
+        this.bo = bo;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isBo() {
+        return bo;
+    }
+    public void setBo(boolean bo) {
+        this.bo = bo;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ",bo=" + bo +
+                '}';
+    }
+}
+
